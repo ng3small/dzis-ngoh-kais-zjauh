90d3090e0cb18772ca3809e12c0abac237c6e1c8
ng3small <123550365+ng3small@users.noreply.github.com>
2025-08-04 21:42:18 +0800

    S1/0/2

--- enggame/js/guess_c.js
+++ enggame/js/guess_c.js
@@ -54,14 +54,14 @@ function press_button(ch){
             box_check.style.color='red';
             const buttons=document.querySelectorAll("button");
             buttons.forEach(btn=>{
-                //if (btn.id==="option_"+ch){
-                //    btn.classList.add("flash");
-                //    setTimeout(()=>btn.classList.remove("flash"),500);
-                //}
-                if (btn.id==="option_"+toString(cor)){
-                    btn.classList.add("flash_right");
-                    setTimeout(()=>btn.classList.remove("flash_right"),500);
+                if (btn.id==="option_"+ch){
+                    btn.classList.add("flash");
+                    setTimeout(()=>btn.classList.remove("flash"),500);
                 }
+                //if (btn.id==="option_"+toString(cor)){
+                //    btn.classList.add("flash_right");
+                //    setTimeout(()=>btn.classList.remove("flash_right"),500);
+                //}
             });
         };
         rate=Math.round(right/num*100)
